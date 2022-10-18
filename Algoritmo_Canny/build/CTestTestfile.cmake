7e5486dcc1184e342a7c626668f59808edd29028
Lesly Fuentes <lesly.fuentes@sansano.usm.cl>
2022-10-18 12:27:35 -0300

    Algoritmo Canny
    Algoritmo Canny para detección de malla

---
+++ Algoritmo_Canny/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/lesly.fuentes/Desktop/U/PDI/AyudantiaPDI2022-2-main/ESTUDIO PDI
+# Build directory: C:/Users/lesly.fuentes/Desktop/U/PDI/AyudantiaPDI2022-2-main/ESTUDIO PDI/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
