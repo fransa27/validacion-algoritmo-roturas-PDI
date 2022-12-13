749c857340bb7d1d0435fff34a55092c1487e889
Lesly Fuentes <lesly.fuentes@sansano.usm.cl>
2022-12-12 23:20:25 -0300

    Limpieza de rama

--- Algoritmo_Canny/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/lesly.fuentes/Desktop/U/PDI/AyudantiaPDI2022-2-main/ESTUDIO PDI
-# Build directory: C:/Users/lesly.fuentes/Desktop/U/PDI/AyudantiaPDI2022-2-main/ESTUDIO PDI/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
